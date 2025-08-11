98f237f88cbfebb691dd73ecac0290874db12897
y-syo <mmoussou@student.42angouleme.fr>
2025-08-11 14:30:35 +0200

    「🔨」 fix(wordpress/entrypoint): removed useless log

--- srcs/wordpress/scripts/entrypoint.sh
+++ srcs/wordpress/scripts/entrypoint.sh
@@ -17,7 +17,6 @@ else
 	echo "Confiruring wordpress..."
 	wp --allow-root core config --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_DB_PASS --dbhost=$WP_DB_HOST --dbprefix=wp_ --path=$WP_DIR
 	wp --allow-root core install --url=https://$WP_URL --title="$WP_TITLE" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --path=$WP_DIR
-	echo "wp user create $WP_USER $WP_USER_EMAIL --role=$WP_USER_ROLE --user_pass=$WP_USER_PASS"
 	wp user create $WP_USER $WP_USER_EMAIL --role=$WP_USER_ROLE --user_pass=$WP_USER_PASS
 	wp option update blog_public $WP_SEARCH_ENGINE_VISIBILITY --allow-root
 fi
